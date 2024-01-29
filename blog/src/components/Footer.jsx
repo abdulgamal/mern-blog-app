9189904f97cf1a30d10aa1a370a478e5cdde059a
Abdul Gamal <abdul.nanjira@gmail.com>
2024-01-29 18:12:56 +0300

    footer links

--- blog/src/components/Footer.jsx
+++ blog/src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 function FooterComponent() {
   return (
     <Footer container className="container mx-auto">
       <Footer.Copyright href="#" by="Blog" year={2024} />
       <Footer.LinkGroup>
-        <Footer.Link href="#">Home</Footer.Link>
-        <Footer.Link href="#">Create</Footer.Link>
+        <Footer.Link as={"div"}>
+          <Link to="/">Home</Link>
+        </Footer.Link>
+        <Footer.Link as={"div"}>
+          <Link to="/create">Create</Link>
+        </Footer.Link>
       </Footer.LinkGroup>
     </Footer>
   );
