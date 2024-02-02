47f95543dbdd5101a848e20a9849609a93078d82
Abdul Gamal <abdul.nanjira@gmail.com>
2024-02-02 15:12:10 +0300

    hash password, generate token and login functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new Schema(
       default:
         "https://images.unsplash.com/photo-1706553458726-36db69e4f817?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D",
     },
+    isAdmin: {
+      type: "Boolean",
+      default: false,
+    },
   },
   { timestamps: true }
 );
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,16 +1,47 @@
 const express = require("express");
 const User = require("../models/user.model");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const { username, email, password, profile_image } = req.body;
-  let newObj = { username, email, password, profile_image };
+
+  const hashPwd = await bcrypt.hash(password, 10);
+  let newObj = { username, email, password: hashPwd, profile_image };
 
   try {
     const newUser = await User.create(newObj);
-    res.json(newUser);
+    const token = jwt.sign(
+      { userId: newUser._id, isAdmin: newUser.isAdmin },
+      process.env.SECRET_JWT
+    );
+    const { password, ...others } = newUser._doc;
+    res.cookie("access_token", token).json(others);
   } catch (error) {
     console.log(error);
+    res.json({ error: error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password: pwd } = req.body;
+  try {
+    const loggedUser = await User.findOne({ username });
+    const isSamePassword = await bcrypt.compare(pwd, loggedUser.password);
+    if (!loggedUser) {
+      return res.json({ message: "Wrong credentials" });
+    }
+    if (!isSamePassword) {
+      return res.json({ message: "Wrong credentials!" });
+    }
+    const token = jwt.sign(
+      { userId: loggedUser._id, isAdmin: loggedUser.isAdmin },
+      process.env.SECRET_JWT
+    );
+    const { password, ...others } = loggedUser._doc;
+    res.cookie("access_token", token).json(others);
+  } catch (error) {
     res.json({ error: error });
   }
 });
