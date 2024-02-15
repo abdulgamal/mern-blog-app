0000ffa09d76400be82bfda4248b30f2a47e68ad
Abdul Gamal <abdul.nanjira@gmail.com>
2024-02-15 12:59:34 +0300

    persist user with redux persist

--- blog/app/store.js
+++ blog/app/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../src/features/users/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: { user: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
