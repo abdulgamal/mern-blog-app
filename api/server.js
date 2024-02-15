fc97709da511ec11200cd29fcf79408a7b426ca8
Abdul Gamal <abdul.nanjira@gmail.com>
2024-02-15 22:52:30 +0300

    api changes

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_PROD],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
