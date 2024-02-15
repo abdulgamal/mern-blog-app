29e605942f3c1bebb04af2162784d38b07fbce95
Abdul Gamal <abdul.nanjira@gmail.com>
2024-02-15 22:55:23 +0300

    cors error fix

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_PROD,
     credentials: true,
   })
 );
